98cd131718d1d4a6c5ca43a45dac9762eca269a1
Aju George <ajimsjames@gmail.com>
2026-07-27 22:20:59 +0530

    Release v1.4.0: Add GPX Trail Navigation Loader & Off-Line Hiking Routes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.wearmaps"
         minSdk = 30
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
 
         vectorDrawables {
             useSupportLibrary = true
