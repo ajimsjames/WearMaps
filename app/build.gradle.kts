8ba25e59f2f58c43fd071d07192a2e6b13277f0b
Aju George <ajimsjames@gmail.com>
2026-07-28 16:02:43 +0530

    Release v2.2.0: Set correct versionCode to 4 and versionName to 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.ajimsjames.wearmaps"
         minSdk = 30
         targetSdk = 33
-        versionCode = 7
+        versionCode = 4
         versionName = "2.2.0"
 
         vectorDrawables {
